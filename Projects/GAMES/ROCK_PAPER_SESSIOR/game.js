16d8b862907765eb96afc407721216e9c608f2d9
Abhijeet Gupta <abhijeet.sci02@gmail.com>
2025-12-22 20:04:45 +0530

    COMPLETED ROCK PAPER SESSIOR GAME

--- Projects/GAMES/ROCK_PAPER_SESSIOR/game.js
+++ Projects/GAMES/ROCK_PAPER_SESSIOR/game.js
@@ -1,10 +1,73 @@
-let button=document.querySelector('button');
-let rock=document.querySelector('rockimg');
-let paper=document.querySelector('paperimg');
-let sessior=document.querySelector('sessiorimg');
+let choices=document.querySelectorAll(".choice");
+let winner=document.querySelector("#whowon");
+let count=document.querySelector("#scorecount");
+let resetbutton=document.querySelector(".resetbutton");
+let userscore=0;
+let comscore=0;
 
-let choices={"rock":1,"paper ":2,"sessior":3};
-let comp=Math.floor(Math.random()*3); //computer chooses the number
-let computerchoice={1:"rock",2:"paper",3:"sessior"}
-console.log("Your choice",choices);
-console.log("Computer choice",comp);
+
+// Generating Computer Choice
+
+function computerchoice() 
+{   
+    const arraychoice =["rock","paper","scissor"];
+    let randomnumber=Math.floor(Math.random()*3);
+    return arraychoice[randomnumber];
+}
+
+
+
+//Printing User choice 
+function userchoice (choice)
+{
+    const choiceId = choice.getAttribute("id");
+    const computerchoiceId=computerchoice();
+    console.log ("User Choice is ",choiceId);
+    console.log("Computer Choose",computerchoiceId);
+    playgame (choiceId,computerchoiceId);
+    return choiceId;
+}
+choices.forEach((choice) => {
+choice.addEventListener("click", () => userchoice(choice));}
+);
+
+
+// Playing Game 
+
+const playgame = (choiceId,computerchoiceId) =>
+{
+    
+    if(computerchoiceId === choiceId)
+    {
+        console.log ("Draw");
+        winner.innerText=" It's a DRAW";
+    }
+    else if((computerchoiceId=="rock" && choiceId== "paper") || (computerchoiceId == "paper" && choiceId=="scissor") || (computerchoiceId==="scissor" && choiceId=== "rock"))
+    {
+        console.log("User Wins");
+        winner.innerText="PLAYER WINS";
+        userscore=userscore+1;
+       count.innerText="Player :"+userscore +"  Computer :"+ comscore;
+
+    }
+    else
+    {
+        console.log("Computer Wins");
+        winner.innerText="COMPUTER WINS";
+         comscore=comscore+1;
+       count.innerText="Player :"+userscore +"Computer :"+ comscore;
+    }
+}
+
+
+// Resetting The Score When 
+ function resetting ()
+ {
+    userscore=0;
+    comscore=0;
+     count.innerText="Player :"+userscore +"Computer :"+ comscore;
+ }
+ resetbutton.addEventListener("click" , () => resetting ());
+
+
+
